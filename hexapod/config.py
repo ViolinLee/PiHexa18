2a675fd1d50717a551dd1939e1d8ed151aadeeb1
ViolinLee <mechleechan@outlook.com>
2020-07-20 20:38:45 +0800

    add robot config file

---
+++ hexapod/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# mounting position
+leg_mount_left_right_x = 29.87  # position in x direction of the middle legs
+leg_mount_other_x = 22.41  # position in x direction of the fore and hind legs
+leg_mount_other_y = 55.41  # position in y direction of the fore and hind legs
+
+# link length
+leg_root2joint1 = 20.75
+leg_joint1_2joint2 = 28.0
+leg_joint2_2joint3 = 42.6
+leg_joint3_2tip = 89.07
+
+# movement parameters
+movement_interval = 5
+movement_switch_duration = 150
