12d54a550578b66e7bfa0d4cd8c201ba0c72ffa9
ViolinLee <mechleechan@outlook.com>
2020-07-22 23:21:07 +0800

    define some calculation

--- hexapod/math_utils.py
+++ hexapod/math_utils.py
@@ -4,32 +4,36 @@ from base import *
 
 """数学计算相关宏定义"""
 
-sin45 = 0.7071
-cos45 = 0.7071
+SIN30 = 0.5
+COS30 = 0.866
+SIN45 = 0.7071
+COS45 = 0.7071
+SIN15 = 0.2588
+COS15 = 0.9659
 
 
-def rotate0(src):
+def rotate0(src: point3d):
     dest = point3d(src.x, src.y, src.z)
     return dest
 
 
-def rotate45(src):
+def rotate45(src: point3d):
     dest = point3d()
-    dest.x = src.x * cos45 - src.y * sin45
-    dest.y = src.x * sin45 + src.y * cos45
+    dest.x = src.x * COS45 - src.y * SIN45
+    dest.y = src.x * SIN45 + src.y * COS45
     dest.z = src.z
     return dest
 
 
-def rotate135(src):
+def rotate135(src: point3d):
     dest = point3d()
-    dest.x = src.x * -cos45 - src.y * sin45
-    dest.y = src.x * sin45 + src.y * -cos45
+    dest.x = src.x * -COS45 - src.y * SIN45
+    dest.y = src.x * SIN45 + src.y * -COS45
     dest.z = src.z
     return dest
 
 
-def rotate180(src):
+def rotate180(src: point3d):
     dest = point3d()
     dest.x = -src.x
     dest.y = -src.x
@@ -37,24 +41,23 @@ def rotate180(src):
     return dest
 
 
-def rotate225(src):
+def rotate225(src: point3d):
     dest = point3d()
-    dest.x = src.x * -cos45 - src.y * -sin45
-    dest.y = src.x * -sin45 + src.y * -cos45
+    dest.x = src.x * -COS45 - src.y * -SIN45
+    dest.y = src.x * -SIN45 + src.y * -COS45
     dest.z = src.z
     return dest
 
 
-def rotate315(src):
+def rotate315(src: point3d):
     dest = point3d()
-    dest.x = src.x * cos45 - src.y * -sin45
-    dest.y = src.x * -sin45 + src.y * cos45
+    dest.x = src.x * COS45 - src.y * -SIN45
+    dest.y = src.x * -SIN45 + src.y * COS45
     dest.z = src.z
     return dest
 
 
 if __name__ == '__main__':
-
-
-
-
+    src = point3d(5, 5, 0)
+    dest = rotate45(src)
+    print(dest.x, dest.y, dest.z)
