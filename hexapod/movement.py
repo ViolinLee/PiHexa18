f3384eaae2846b6ece3260b81ea3572bd3a6ab3e
ViolinLee <mechleechan@outlook.com>
2020-07-30 00:51:44 +0800

    add movement process

--- hexapod/movement.py
+++ hexapod/movement.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
-from base import *
+from movement_table import *
+from movements import *
+from random import randint
 
 """
 定义六足机器人移动相关函数。
@@ -27,16 +29,64 @@ class MovementMode(Enum):
     MOVEMENT_TOTAL = 13
 
 
+k_table = [standby_table,
+           forward_table,
+           forward_fast_table,
+           backward_table,
+           turn_left_table,
+           turn_right_table,
+           shift_left_table,
+           shift_right_table,
+           climb_table,
+           rotate_x_table,
+           rotate_y_table,
+           rotate_z_table,
+           twist_table]
+
+
 class Movement(object):
-    def __init__(self, mode: MovementMode):
+    def __init__(self, mode, transiting: bool):
         self.__mode = mode
-        self.__position = locations()
-        self.__index = 0
-        self.__transiting = False
+        self.__position = k_standby
+        self.__index = 0  # index in mode position table
+        self.__transiting = transiting  # if still in transiting to new mode
         self.__remain_time = 0
 
     def set_mode(self, new_mode):
-        return
+        if not k_table[new_mode].entries:
+            # log_info
+            return
+
+        self.__mode = new_mode
+
+        table = k_table[self.__mode]
+
+        self.__index = table.entries[randint() % table.entries_count]  # count==1:__index=0/1; count==2: __index=0/1/2
+        self.__remain_time = movement_switch_duration if movement_switch_duration > table.step_duration else table.step_duration
+
+    def next(self, elapsed):
+        table = k_table[self.__mode]
+
+        if elapsed <= 0:
+            elapsed = table.step_duration
+
+        if self.__remain_time <= 0:
+            self.__index = (self.__index + 1) % table.length
+
+        if elapsed >= self.__remain_time:
+            elapsed = self.__remain_time
+
+        ratio = elapsed / self.__remain_time
+        self.__position += (table[self.__index] - self.__position) * ratio
+        self.__remain_time -= elapsed
+
+        return self.__position
 
 
-
+class MovementTable(object):
+    def __init__(self, table, length, step_duration, entries, entries_count):
+        self.table = table
+        self.length = length
+        self.step_duration = step_duration
+        self.entries = entries
+        self.entries_count = entries_count
